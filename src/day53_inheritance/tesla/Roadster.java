b2759f51673de4f535b0fbd5689973e45ea30347
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-06-13 12:02:00 -0500

    - added ModelX and Objects class

--- src/day53_inheritance/tesla/Roadster.java
+++ src/day53_inheritance/tesla/Roadster.java
@@ -1,8 +1,11 @@
 package day53_inheritance.tesla;
 
 public class Roadster extends ElectricCar {
+    //Roadster r = new Roadster("Roadster", 200000, 2022, 620);
     public Roadster(String model, double price, int year, int range) {
+        //"Tesla", "Roadster", 200000, 2022, 620
         super("Tesla", model, price, year, range);
+        System.out.println("Welcome new Roadster");
     }
 
 }
