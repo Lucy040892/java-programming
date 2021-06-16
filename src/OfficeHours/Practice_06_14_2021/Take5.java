99307975c12788cf87ad11867d5b9c53b9e1bec1
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-06-16 11:50:19 -0500

    added new office hours package

---
+++ src/OfficeHours/Practice_06_14_2021/Take5.java
@@ -0,0 +1,9 @@
+package OfficeHours.Practice_06_14_2021;
+
+public class Take5 extends Candy{
+
+    public Take5(int quantity, boolean hasPeanuts) {
+        super("Reese", quantity, hasPeanuts);
+    }
+
+}
