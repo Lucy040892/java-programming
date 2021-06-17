6f8fc93232d56a7c66d133544e8a43c78a58e9b9
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-06-16 21:49:48 -0500

    day54 abstraction

---
+++ src/day54_abstraction/Student.java
@@ -0,0 +1,22 @@
+package day54_abstraction;
+/*-> we add abstract keyword to a class to make it an abstract class.
+-> We can not create object of student class - meaning:
+    Student student = new Student(); will show ERROR
+-> what can we do with this Student class? We can extend this class by sub classes
+-> Student class will be Parent class for all other types of students related classes
+ */
+
+public abstract class Student {
+    public void code(String language) {
+        System.out.println("Student is coding using " +language);
+    }
+    /*
+    we can add abstract methods into abstract class.
+    abstract method -> is created using abstract keyword
+    and does not have the implementation/method body
+    So abstract class only shows WHAT it can do, but not HOW by using abstract methods.
+     */
+    public abstract void attendClass();
+
+    }
+
