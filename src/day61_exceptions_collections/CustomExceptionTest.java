03d8b7f0ea57991af986931f1109aa970c06a402
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-07-11 14:04:38 -0500

    day 61 java

---
+++ src/day61_exceptions_collections/CustomExceptionTest.java
@@ -0,0 +1,20 @@
+package day61_exceptions_collections;
+
+public class CustomExceptionTest {
+    public static void main(String[] args) {
+        int minutes = 30;
+        System.out.println("Class is going on for " +minutes +" minutes");
+        if(minutes > 50) {
+            //throw new BreakTimeException();//we create custom "BreakTimeException" by adding class, which extends RuntimeException
+        throw new BreakTimeException("It is Break Time!");
+        }
+        System.out.println("Let's continue the class");
+
+        double balance = 400;
+        double itemPrice = 500;
+        if(itemPrice > balance) {
+            throw new InsufficientBalanceException("Transaction declined. Not enough founds");
+        }
+        System.out.println("Transaction is successful");
+    }
+}
---
+++ src/day61_exceptions_collections/ElementaryStudent.java
@@ -0,0 +1,45 @@
+package day61_exceptions_collections;
+
+public class ElementaryStudent {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        //check if name is null first, otherwise we can get nullpointer exception
+        if(name == null || name.isEmpty()) {
+           throw new IllegalArgumentException("Name cannot be empty or null");
+        }
+        //if name is all number or starts with numbers
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ElementaryStudent() {} //empty constructor
+
+    public ElementaryStudent(String name, int age) { //constructor with arguments
+        setName(name);
+        setAge(age);
+    }
+
+    @Override
+    public String toString() {
+        return "ElementaryStudent{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setAge(int age) {
+        if(age < 5 || age > 12) {
+            throw new IllegalArgumentException("Age cannot be < 5 or > 12");
+        }
+        this.age = age;
+    }
+}
