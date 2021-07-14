006dbd7a4b835cd328d4420de593315a8231c0ad
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-07-13 21:17:45 -0500

    day 62 java

---
+++ src/OfficeHours/Practice_07_12_2021/PI.java
@@ -0,0 +1,6 @@
+package OfficeHours.Practice_07_12_2021;
+
+
+public interface PI {
+    void math();
+}
---
+++ src/day62_collections/VectorDemo.java
@@ -0,0 +1,16 @@
+package day62_collections;
+import java.util.*;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<Integer> nums = new Vector<>();
+        List<String> states = new Vector<>();
+        states.add("VA");
+        states.add("IL");
+        states.add("FL");
+        states.add("VA");
+        System.out.println(states);
+        //VECTOR is synchronized
+
+    }
+}
